2f69b3b62df642f9fd8dcacb3790269fb9390dce
root <root@DESKTOP-9KQVLGB>
2024-01-07 00:14:21 -0300

    exercicios da facul add

--- exercicios.c
+++ exercicios.c
@@ -445,3 +445,222 @@ int main (void)
     printf("/");
     printf("\n"); 
 }
+
+/* 
+
+
+Daqui pra baixo são algumas atividades antigas da faculdade, fiz no terceiro semestre,
+talvez possuam erros, identaçoes terriveis, mas nao vou ficar refazendo
+
+
+*/
+
+#include<stdio.h>
+int main(){
+	int nota[10],i;
+	for(i=0;i<10;i++){
+		printf("%d - Digite as notas: ",i+1);
+		scanf("%d",&nota[i]);
+	}
+	
+	printf("Notas maiores que 6: \n");
+	for(i=0;i<10;i++){
+		if(nota[i]>=6){
+			printf("%d\n",nota[i]);
+		}
+	}
+}
+
+//
+
+#include <stdio.h>
+#define max 100
+int main() {
+	int n,i,vez,j;
+	float vet[max];
+	printf("Quantos elementos tera o vetor: ");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++){
+		printf("%d - Digite seus numeros reais: ",i+1);
+		scanf("%f",&vet[i]);
+	}
+	for(i=0;i<n;i++){
+		vez=1;
+		j=i+1;
+		while(j<n){
+			if(vet[j] != vet[i]){
+				j++;
+			}else{
+				vez++;
+				n--;
+				vet[j] = vet[n];
+			}
+		}
+   		printf("%.2f ocorre %d vezes\n", vet[i], vez);
+	}
+	return 0;
+}
+
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+main(){
+    int x,y,tam;
+    float vet[10], quad[10];
+    for (x=0; x<=9; x++)
+ {
+    printf("Digite o numero %d: ",x+1);
+    scanf("%f", &vet[x]);
+    quad[x] = pow(vet[x],2);
+ }
+    printf("\n\nVetor:    ");
+    for (x=0; x<=9;x++)
+     printf("%4.0f",vet[x]);
+      printf("\n\n");
+       printf("Quadrado: ");
+
+    for (x=0; x<=9;x++)
+    printf("%4.0f",quad[x]);
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
+
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int a, maior=0, menor=0;
+    float valor[15];
+
+    printf("digite 15 valores: ");
+    printf("\n");
+    for (a=0;a<15;a++){
+      printf("valor %d: ", a+1);
+      scanf("%f", &valor[a]);
+      if(a==0){maior=valor[a];menor=valor[a];}
+      if(valor[a]>maior){
+        maior=valor[a];
+      }
+      else{
+        if(valor[a]<menor){
+            menor=valor[a];
+        }
+      }
+    }
+    printf("\nSeu maior valor foi: %d\n", maior);
+    printf("\nSeu menor valor foi: %d\n\n", menor);
+    system("pause");
+    return 0;
+}
+
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int vet[9], i, p=0, n=0;
+    for(i=0;i<=9;i++)
+{
+    printf("Informe o valor do vetor %i: ",i+1);
+    scanf("%i", &vet[i]);
+    if(vet[i]>0)
+    p = p +vet[i];
+    if(vet[i]<0,n);
+}
+    printf("Soma dos numeros positivos = %d\n", p);
+    printf("Quantidade de numeros negativos = %d\n", n);
+    return 0;
+}
+
+// bhaskara
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+int a,b,c,d,x1,x2;
+printf("A:");
+scanf("%d",&a);
+printf("B:");
+scanf("%d",&b);
+printf("C:");
+scanf("%d",&c);
+d=pow(b,2)-(4*a*c);
+printf("Delta=%d\n",d);
+if(d<0)
+printf("Nao pertence aos numeros reais");
+else
+{
+	x1=(-b+sqrt(d))/(2*a);
+	x2=(-b-sqrt(d))/(2*a);
+	printf("X1=%d\n",x1);
+	printf("X2=%d\n",x2);
+}
+getch();
+}
+
+// media
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	float n1,n2,n3,maior,menor,media;
+	printf("Valor 1:");
+	scanf("%f",&n1);
+	printf("Valor 2:");
+	scanf("%f",&n2);
+	printf("Valor 3:");
+	scanf("%f",&n3);
+	media=(n1+n2+n3)/3;
+	printf("Media =%.2f\n",media);
+//achar menor
+	if (n1<=n2 && n1<=n3)
+		menor=n1;
+	else if(n2<=n1 && n2<=n3)
+		menor=n2;
+	else
+		menor=n3;
+	//achar maior
+		if (n1>=n2 && n1>=n3)
+		maior=n1;
+	else if(n2>=n1 && n2>=n3)
+		maior=n2;
+	else
+		maior=n3;
+//mostrar menor e maior
+	printf("Menor=%.2f\n",menor);
+	printf("Maior=%.2f\n",maior);
+}
+
+// triangulo
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int triangulo;
+	printf("Quantos lados iguais tem seu triangulo?");
+	scanf("%d",&triangulo);
+	if (triangulo>2 && triangulo<4)
+	printf("Seu triangulo e equilatero");
+	else if (triangulo==2)
+	printf("Seu triangulo e isosceles");
+	else if (triangulo==0)
+	printf("Seu triangulo e escaleno");
+	else
+	printf("Numero invalido, por favor, repita o processo");
+	getch();
+}
+
+//
+
